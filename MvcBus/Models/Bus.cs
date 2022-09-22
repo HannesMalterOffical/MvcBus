b681faed9327dc9a929967f746e2d94042c27ff1
opilane <opilane@HP-CZC6297B8G>
2022-09-22 14:08:09 +0300

    Add project files.

---
+++ MvcBus/Models/Bus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBus.Models
+{
+    public class Bus
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Arrival { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
